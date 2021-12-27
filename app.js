f00f928ba21284bb41e73a7f884dd1ca6f2e3fff
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2021-12-27 22:38:39 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// This creates a vue app and assigns it to the app variable
+const app = Vue.createApp({
+  // any data, functions to react to events that we want
+  // component template can be specified here
+  //  creating a template property
+  //template: "<h2>I am the template</h2>",
+});
+
+// mounting is the process of outputting the virtual
+// representation of a component into the final UI representation
+app.mount("#app");
