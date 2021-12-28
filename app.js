7ed020eaf92a9ddf5303944da1d076eb290d363e
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2021-12-28 20:59:22 +0100

    Lesson 11: Other Mouse Events

--- app.js
+++ app.js
@@ -10,12 +10,24 @@ const app = Vue.createApp({
       title: "The Final Empire",
       author: "Brandon Sanderson",
       age: 45,
+      x: 0,
+      y: 0,
     };
   },
   methods: {
     toggleShowBooks() {
       //!this.showBooks means when its the opposite of what it is
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e, e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
     },
   },
 });
