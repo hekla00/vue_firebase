07196a512af98c2858df712320fc283f186ae9b3
Hekla Alexía Samúelsdóttir <hekla@villingaholt.nu>
2021-12-28 21:17:28 +0100

    Lesson 12: Outputting Lists (v-for)

--- app.js
+++ app.js
@@ -7,27 +7,26 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: "The Final Empire",
-      author: "Brandon Sanderson",
-      age: 45,
-      x: 0,
-      y: 0,
+      books: [
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+        },
+        {
+          title: "the way of kings",
+          author: "brandon sanderson",
+        },
+        {
+          title: "the final empire",
+          author: "brandon sanderson",
+        },
+      ],
     };
   },
   methods: {
     toggleShowBooks() {
       //!this.showBooks means when its the opposite of what it is
       this.showBooks = !this.showBooks;
-    },
-    handleEvent(e, data) {
-      console.log(e, e.type);
-      if (data) {
-        console.log(data);
-      }
-    },
-    handleMousemove(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
     },
   },
 });
